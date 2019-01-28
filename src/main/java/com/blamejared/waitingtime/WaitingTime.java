e7bcfca4376291e452ea0c9172066945477f820f
Jared <jaredlll08@gmail.com>
2019-01-28 23:03:41 +0200

    close #4

--- src/main/java/com/blamejared/waitingtime/WaitingTime.java
+++ src/main/java/com/blamejared/waitingtime/WaitingTime.java
@@ -4,11 +4,12 @@ import net.minecraftforge.fml.common.*;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import org.apache.logging.log4j.LogManager;
 
-@Mod(modid = "waitingtime", certificateFingerprint = "6919c86a9d8d117dbc520ba31378675eaae1c16a")
+@Mod(modid = "waitingtime", certificateFingerprint = "6919c86a9d8d117dbc520ba31378675eaae1c16a", clientSideOnly = true)
 public class WaitingTime {
     
     @Mod.EventHandler
     public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
         LogManager.getLogger("waitingtime").warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
+        
     }
 }
